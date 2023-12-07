5f84d5d891b6ef5ed944307af6fcbb15810d3fea
Don Paul <donpaulmaj@gmail.com>
2023-12-06 20:54:05 -0500

    Added some content on the main page. Added normalize.css npm package to the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <main>
       <h1>The Majeggstics</h1>
-    </>
+      <section>
+        <div>
+          <h3>
+            Welcome to The Majeggstics, where No Farmer is Left Behind!
+          </h3>
+          <p>
+            Majeggstics is a worldwide group of Egg Inc. players with the common goal of making sure every player can complete every contract, by optimizing play strategies and making fair contributions to our co-ops.
+          </p>
+          <br />
+          <p>
+            How to join our group? <br />
+
+            You&apos;ll need to join the Egg Inc. discord server at the link below and <abbr title="Direct Message">DM</abbr> one of our recruiters (@placeholder).
+          </p>
+          <br />
+          <p>
+            <a href="https://discord.gg/egginc">Egg Inc. discord</a>
+          </p>
+        </div>
+      </section>
+    </main>
   )
 }
 
