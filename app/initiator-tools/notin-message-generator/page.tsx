4866dd47aa59a3ca2fd9e90228b74304ac3ee74c
DukeCephalopod <contact@dukeceph.xyz>
2024-09-18 14:38:54 -0500

    refactor: overhaul and test notin-message-generator

--- app/initiator-tools/notin-message-generator/page.tsx
+++ app/initiator-tools/notin-message-generator/page.tsx
@@ -6,70 +6,72 @@ import moment from 'moment-timezone';
 import { Fragment, useState } from 'react';
 import ToastMessage, { notify } from '@/components/ToastMessage';
 
-interface ThreadMatch {
+interface NotIn {
 	threadUrl: string | null;
 	user: string;
 }
 
-const convertToDiscordUrl = (url: string) => {
-	const match = /discord.com\/channels\/(\d+)\/(\d+)/.exec(url);
+export const enum Timeslot {
+	One,
+	Two,
+	Three,
+}
+
+const timeslotMap = {
+	emoji: [':one:', ':two:', ':three:'],
+	eggst: ['+1', '+6', '+12'],
+	header: ['Timeslot 1', 'Timeslot 2', 'Timeslot 3'],
+	hour: [18, 23, 5], // hours in America/Toronto
+} as const;
+
+const slotFormat = (timeslot: Timeslot, format: keyof timeslotMap) => timeslotMap[format][timeslot];
+
+export const convertToDiscordUrl = (url: string) => {
+	const match = /discord.com\/channels\/(?<channelId>\d+)\/(?<messageId>\d+)/.exec(url);
 	if (match) {
-		const [_, channelId, messageId] = match;
+		const { channelId, messageId } = match.groups;
 		return `discord://-/channels/${channelId}/${messageId}`;
 	}
 
 	return url;
 };
 
+const zipRegex = (res: RegExp[], flags: string) => new RegExp(res.map((re) => re.source).join(''), flags);
+
+export const parseNotInMessage = (input: string): NotIn[] => {
+	const timeslotRegex = zipRegex(
+		[/Timeslot\s*(?<timeslotEmoji>:[a-z]+:):/, /(?<notins>(?:.|\n)+?)/, /(?=Timeslot|\(no pings were sent\)|$)/],
+		'g',
+	);
+	const matches = [...input.matchAll(timeslotRegex)];
+
+	return matches.flatMap((match) => {
+		const emoji = match.groups.timeslotEmoji;
+		const timeslot = timeslotMap.emoji.indexOf(emoji);
+		if (timeslot === -1) return null;
+
+		return match.groups.notins
+			.trim()
+			.split('\n')
+			.flatMap((line) => {
+				const threadMatch = /\[thread]\(<(?<url>[^>]+)>\)/.exec(line);
+				const threadUrl = threadMatch ? convertToDiscordUrl(threadMatch.groups.url.trim()) : null;
+				const userMatches = line.match(/<@\d+> \(`[^)]+`\)/g);
+
+				return userMatches.map(
+					(user: string): NotIn => ({
+						timeslot,
+						user,
+						threadUrl,
+					}),
+				);
+			});
+	});
+};
+
 export default function NotInMessageGeneratorPage() {
 	const [notInMessage, setNotInMessage] = useState('');
 	const [copiedElements, setCopiedElements] = useState<{ [key: number]: { [key: number]: boolean } }>({});
-
-	const timeslotMap: Record<string, string> = {
-		':one:': '+1',
-		':two:': '+6',
-		':three:': '+12',
-	};
-
-	const timeslotHeaderMap: Record<string, string> = {
-		'+1': 'Timeslot 1',
-		'+6': 'Timeslot 2',
-		'+12': 'Timeslot 3',
-	};
-
-	const parseInput = (input: string) => {
-		const timeslotRegex = /Timeslot\s:([a-z]+)::\n((?:.|\n)+?)(?=(Timeslot\s:[a-z]+::|\(no pings were sent\)|$))/g;
-		const matches = [...input.matchAll(timeslotRegex)];
-
-		return matches.reduce<Record<string, ThreadMatch[]>>((acc, match) => {
-			const emoji = `:${match[1]}:`;
-			const timeslot = timeslotMap[emoji] || '';
-			const content: ThreadMatch[] = match[2]
-				.trim()
-				.split('\n')
-				.flatMap((line) => {
-					// Extract thread URL and all user mentions
-					const threadMatch = /\[thread]\(<([^>]+)>\)/.exec(line);
-					const userMatches = [...line.matchAll(/<@\d+> \(`[^)]+`\)/g)].join(',');
-
-					if (userMatches.length > 0) {
-						return {
-							user: userMatches,
-							threadUrl: threadMatch ? convertToDiscordUrl(threadMatch[1].trim()) : null,
-						};
-					}
-
-					return null;
-				})
-				.filter<ThreadMatch>((thread): thread is ThreadMatch => thread !== null);
-
-			if (timeslot) {
-				acc[timeslot] = (acc[timeslot] || ([] as ThreadMatch[])).concat(content);
-			}
-
-			return acc;
-		}, {});
-	};
 
 	const copyToClipboard = (text: string, timeslotIndex: number, userIndex: number): void => {
 		const copyResult = copy(text);
@@ -88,30 +90,24 @@ export default function NotInMessageGeneratorPage() {
 	};
 
 	const generateOutput = () => {
-		const timeslotGroups = parseInput(notInMessage);
+		const notins = parseNotInMessage(notInMessage);
 		const outputElements: ReturnType<typeof Fragment>[] = [];
 
-		for (const [timeslotIndex, [timeslot, users]] of Object.entries(timeslotGroups).entries()) {
-			const hourMap = { '+1': 18, '+6': 23, '+12': 5 };
-			// TODO: this cast is stupid, proper typing will fix it
-			const hour = hourMap[timeslot as '+1' | '+6' | '+12'];
+		for (const timeslot of Timeslot) {
+			const hour = slotFormat(timeslot, 'hour');
 			const date = moment().tz('America/Toronto').hour(hour).minute(0).second(0);
-			const now = date.unix();
-			const discordTimestamp = `<t:${now}:t>`;
-
-			// Add a smaller header for each timeslot
-			const timeslotHeader = timeslotHeaderMap[timeslot] || `Timeslot ${timeslot}`;
+			const discordTimestamp = `<t:${date.unix()}:t>`;
 
 			outputElements.push(
-				<Fragment key={`header-${timeslotIndex}`}>
-					<h4>{timeslotHeader}</h4> {/* Changed to h4 for smaller header */}
+				<Fragment key={`header-${timeslot}`}>
+					<h4>{slotFormat(timeslot, 'header')}</h4>
 				</Fragment>,
 			);
 
-			for (const [userIndex, userEntry] of users.entries()) {
+			for (const [userIndex, userEntry] of notins) {
 				const { user, threadUrl } = userEntry;
 				const textToBeCopied = `${user}. Courtesy reminder to join your coop ASAP! You will receive a strike if you don’t join by +${Number.parseInt(timeslot, 10) + 5} (${discordTimestamp} in your time zone).`;
-				const isCopied = copiedElements[timeslotIndex]?.[userIndex];
+				const isCopied = copiedElements[timeslot]?.[userIndex];
 
 				outputElements.push(
 					<Fragment key={`${timeslot}-${userIndex}`}>
@@ -124,7 +120,7 @@ export default function NotInMessageGeneratorPage() {
 							{isCopied ? ' ✅ ' : ' ❌ '}
 							{textToBeCopied}
 						</p>
-						<button onClick={() => copyToClipboard(textToBeCopied, timeslotIndex, userIndex)}>Copy to Clipboard</button>
+						<button onClick={() => copyToClipboard(textToBeCopied, timeslot, userIndex)}>Copy to Clipboard</button>
 					</Fragment>,
 				);
 			}
