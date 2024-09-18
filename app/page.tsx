ef20f9672b779577a41843087ad85dd0cfcf237d
Carter Himmel <me@fyko.net>
2024-09-18 10:06:06 -0600

    chore: unlint [skip-ci] re: https://github.com/CiriaRiannon/majeggstics/pull/10\#discussion_r1765338168 sorry duke

--- app/page.tsx
+++ app/page.tsx
@@ -8,38 +8,28 @@ export default function Home() {
       <h1>The Majeggstics</h1>
       <section>
         <div>
-          <h3>Welcome to The Majeggstics, where No Farmer is Left Behind!</h3>
+          <h3>
+            Welcome to The Majeggstics, where No Farmer is Left Behind!
+          </h3>
           <p>
-            Majeggstics is a worldwide group of Egg Inc. players with the common
-            goal of making sure every player can complete every contract, by
-            optimizing play strategies and making fair contributions to our
-            co-ops.
+            Majeggstics is a worldwide group of Egg Inc. players with the common goal of making sure every player can complete every contract, by optimizing play strategies and making fair contributions to our co-ops.
           </p>
           <br />
           <p>
             How to join our group? <br />
-            You&apos;ll need to join the Egg Inc. discord server at the link
-            below and <abbr title="Direct Message">DM</abbr> one of our
-            recruiters (@placeholder).
+
+            You&apos;ll need to join the Egg Inc. discord server at the link below and <abbr title="Direct Message">DM</abbr> one of our recruiters (@placeholder).
           </p>
           <br />
           <p>
-            <a
-              href="https://discord.gg/egginc"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Egg Inc. discord
-            </a>
+            <a href="https://discord.gg/egginc" target='_blank' rel="noreferrer">Egg Inc. discord</a>
           </p>
 
           <p>
             <Link href="/guide">Guide</Link>
           </p>
           <p>
-            <Link href="/contract-boost-calculator">
-              Contract Boost Calculator
-            </Link>
+            <Link href="/contract-boost-calculator">Contract Boost Calculator</Link>
           </p>
         </div>
       </section>
